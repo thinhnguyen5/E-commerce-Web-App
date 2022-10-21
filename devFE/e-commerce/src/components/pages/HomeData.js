7c1f7673d4cb5752e1c6ee748c133fd0a7f09fca
Thinh Duc <t9ngdu00@students.oamk.fi>
2022-10-21 05:40:19 +0300

    About Us

---
+++ devFE/e-commerce/src/components/pages/HomeData.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: false,
+    topLine: '#STEPOUT COLLECTION | HOLIDAY SEASON 2022',
+    description: 'Through this collection, Bad Habits® encourages the spirit of |Dare to step out, experience new things and explore the lives of young Vietnamese people|.A day of many young people we are still experiencing is extremely boring: Because the same "normal days" are not new at all! Coming to #STEPOUT Collection, it will not be difficult to notice |Vietnamese spirit - Vietnamese culture| clearly visible through design language: From images of Ha Long Bay, Son Doong Cave, Hoi An Ancient Town or Hanoi Turtle Tower - National Scenic Spots - To the emphasis on Pure Vietnamese language"Rong Play" is prominently displayed on the products, which further deepen the Vietnamese quality from the perspective of Streetwear.',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: 'images/img1.jpg',
+    alt: 'Vault'
+};
+
+export const homeObjTwo = {
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: false,
+    topLine: 'ATTENTION TO DETAIL',
+    headline: 'Versatile "Outdoor Material" in product DETAILS #StepOut Collection',
+    description:
+      'Versatile - To make a true #StepOut collection exclusively for Outdoor, "appropriate usability for different outdoor needs" has always been a top priority for Bad Habits® in the development process. product.From each Zips and Strap, every detail & pleated surface, or even the appropriate Discharge printing technique for each #StepOut Collection product has been carefully researched & calculated by Bad Habits® - How to support Support outdoor activities comfortably, but still ensure the quality of everyday Streetwear',
+    buttonLabel: 'See More',
+    imgStart: '',
+    img: 'images/img2.jpg',
+    alt: 'Vault'
+};
+  
+export const homeObjThree = {
+    lightBg: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'ATTENTION TO DETAIL',
+    headline: '6 Fabrics "closer to the International" on #StepOut Collection',
+    description:
+      "It must be the Upgraded Cotton Fabric developed by Bad Habits®, a 100% made in Vietnam weaving process - with the selection of 6 separate Fabrics for #StepOut Collection to suit the characteristics of each product. different products & uses",
+    buttonLabel: 'Start Now',
+    imgStart: 'start',
+    img: 'images/img3.jpg',
+    alt: 'Vault'
+};
+  
+export const homeObjFour = {
+    lightBg: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Bad Habits® Valentine 2022',
+    headline: '"Bad Couple, Good Luv-er"|Good Heart Tee|',
+    description:
+      'With this , Bad Habits wants to give the Baddies-In-Luv a message:"Be always hot, always keep your own personality when together, and always be Bad Couple your way. Because Bad Habits believes that, when you are free to be yourself, everyone will become one. Good Luv-er, a perfect lover in his love"',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: 'images/img4.jpg',
+    alt: 'Vault'
+};
