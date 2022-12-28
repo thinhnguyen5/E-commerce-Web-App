71078b0c24b6eb9a630d333d4d56c6d279dfa9e9
Thinh Duc <t9ngdu00@students.oamk.fi>
2022-12-28 03:33:50 +0200

    Slide image updated

--- src/components/ImageSlider/SliderData.js
+++ src/components/ImageSlider/SliderData.js
@@ -1,11 +1,11 @@
 export const SliderData = [
     {
-        image: 'https://file.hstatic.net/1000351433/file/bn_chinh_0a2b804aaede4ac3baac9fbdd82c7230.jpg'
+        image: 'https://file.hstatic.net/1000351433/file/1._banner_remake_1920x930_c131b61e872344fbb56e089b673835f2.png'
     },
     {
         image: 'https://statics.hnbmg.com/2022/08/31183344/crocs_2-800x600.jpg'
     },
     {
-        image: 'https://file.hstatic.net/1000351433/file/1._banner_remake_1920x930_c131b61e872344fbb56e089b673835f2.png'
+        image: 'https://file.hstatic.net/1000351433/file/bn_chinh_0a2b804aaede4ac3baac9fbdd82c7230.jpg'
     }
 ]
